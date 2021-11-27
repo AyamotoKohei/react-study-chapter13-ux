9ccd09bd943df0480d6a749c1d7539ba048706be
AyamotoKohei <koh14taka2@gmail.com>
2021-11-27 15:23:35 +0900

    add: servicesフォルダの追加

---
+++ staff-list-ux-react-query/src/domains/github/services/delay.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+const delay =
+  (time: number) =>
+  (result: () => any): Promise<unknown> =>
+    new Promise((resolve) => {
+      setTimeout(() => resolve(result), time);
+    });
+
+export default delay;

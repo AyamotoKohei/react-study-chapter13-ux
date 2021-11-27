40a8b0f2d05c94b5bf148b50cacba2494595e07b
AyamotoKohei <koh14taka2@gmail.com>
2021-11-27 17:05:34 +0900

    add: containers/organismsの実装

---
+++ staff-list-ux-react-query/src/containers/organisms/MemberList.tsx
@@ -0,0 +1,19 @@
+import { VFC } from 'react';
+import { useQuery } from 'react-query';
+
+import MemberList from 'components/organisms/MemberList';
+import { getMembers } from 'domains/github';
+
+const EnhancedMemberList: VFC<{ orgCode: string }> = ({ orgCode }) => {
+  const { data: users = [] } = useQuery(
+    [orgCode, 'members'],
+    () => getMembers(orgCode),
+    {
+      enabled: orgCode.length >= 2,
+    },
+  );
+
+  return <MemberList users={users} />;
+};
+
+export default EnhancedMemberList;

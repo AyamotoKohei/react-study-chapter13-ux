5b61346dd4903bb682bbcd2c943a493fdb581502
AyamotoKohei <koh14taka2@gmail.com>
2021-11-28 13:50:17 +0900

    add: indexとAppの実装

--- staff-list-ux-react-query/src/index.tsx
+++ staff-list-ux-react-query/src/index.tsx
@@ -1,17 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createRoot } from 'react-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 
-ReactDOM.render(
-  <React.StrictMode>
+import reportWebVitals from './reportWebVitals';
+import App from './App';
+
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+    },
+    mutations: {
+      retry: 0,
+    },
+  },
+});
+const root = document.getElementById('root') as HTMLElement;
+
+createRoot(root).render(
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    {process.env.NODE_ENV === 'development' && (
+      <ReactQueryDevtools initialIsOpen={false} />
+    )}
+  </QueryClientProvider>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
